b38d25d5c76ab5e163906163cf8209fa5253f59a
Matt Huston <matthuston1@aol.com>
2018-08-29 13:40:43 -0600

    working background image

--- src/App.vue
+++ src/App.vue
@@ -29,8 +29,7 @@ export default {
 }
 body {
 background-image: linear-gradient(rgba(0, 0, 0, 0.60), rgba(0, 0, 0, 0.0)),
-    /* url("https://cdn.pixabay.com/photo/2016/09/26/09/58/school-supplies-1695563_1280.jpg"); */
-    url("https://www.maxpixel.net/static/photo/2x/Plastic-Painting-School-Supplies-Color-Tempera-1695564.jpg");
+  url("./assets/supplyBackground.jpg");
 background-size: 100%;
 background-repeat: no-repeat;
 background-attachment: fixed;
